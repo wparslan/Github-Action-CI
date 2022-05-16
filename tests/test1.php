292a83d8b12a353b9aa2fb12bb9707a166023f64
wparslan <arslan@wpbrigade.com>
2022-05-16 18:11:28 +0500

    update the phpunit testing

---
+++ tests/test1.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+	public function setUp()
+	{
+		// Set up the test class here
+	}
+
+	public function testSomething()
+	{
+		// Write test assertions here
+	}
+
+	public function tearDown()
+	{
+		// Clean up
+	}
+}
